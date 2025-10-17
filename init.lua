9001d34ff88813b626873d89d5e9f14f0b7de692
Trevor Hunnes <thunnes@hotmail.com>
2025-10-16 19:11:46 -0700

    added transparent background

--- init.lua
+++ init.lua
@@ -886,6 +886,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
